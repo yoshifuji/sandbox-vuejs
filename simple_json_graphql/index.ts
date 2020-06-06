5ca80f6b3c96c84f901938d6d180937d64073ff3
yoshifuji <yoshi2430jp@gmail.com>
2020-06-06 23:03:40 +0900

    simple GraphQL Server

---
+++ simple_json_graphql/index.ts
@@ -0,0 +1,30 @@
+import { GraphQLServer } from "graphql-yoga";
+
+const locations = [
+  { Japanese: "三鷹市", Prefecture: "Tokyo", Population: 180797 },
+  { Japanese: "香取市", Prefecture: "Chiba", Population: 85193 },
+];
+
+// Definition
+const typeDefs = `
+    type Location {
+      Japanese: String
+      Prefecture: String
+      Population: Int
+    }
+    type Query {
+      locations: [Location]
+    }
+`;
+
+// Response
+const resolvers = <any>{
+  Query: {
+    locations: () => locations,
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start({ port: 4040 }, () =>
+  console.log(`Your GraphQL server is running now ...`)
+);
